fda6f3d7f3657e74295d422964646f4cbf525609
Ravi Raghav <ravionlineragh@gmail.com>
2015-09-28 12:27:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
-git init
 git config user.name "ravionlineragh"
-git config user.email "1qaz2wsx"
+git config user.email "ravionlineragh@gmail.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
