3cc9be305c932c94f4a4e2c550dd52234a9bdd6e
Ravi Raghav <ravionlineragh@gmail.com>
2015-09-28 11:53:14 +0530

    Update testing.java

--- src/com/test/testing.java
+++ src/com/test/testing.java
@@ -1,9 +1,9 @@
 package com.test;
 
-class testing
-{
-public static void main(String args[]){
-System.out.println("Inside the main event");
-}
+public class Testing {
 
+	public static void main(String args[])
+	{
+		System.out.println("Inside the main class");
+	}
 }
