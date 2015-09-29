df7773b8b3f27924e2e3133a05ef1f34549bab54
Ravi Raghav <ravionlineragh@gmail.com>
2015-09-29 01:27:25 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,1 @@
-git config user.name "ravionlineragh"
-git config user.email "ravionlineragh@gmail.com"
-git add .
-git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+java -jar test.jar
